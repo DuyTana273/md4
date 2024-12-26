1e644b5f192ed69f83a56901527941b993dfcf4c
DuyTana273 <duytana273@gmail.com>
2024-12-26 09:41:06 +0700

    Upload Files

---
+++ customer/demo/src/main/java/com/example/demo/controller/CustomerController.java
@@ -0,0 +1,47 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Customer;
+import com.example.demo.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class CustomerController {
+    @Autowired
+    private ICustomerService customerService;
+
+    @GetMapping()
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/customers")
+    public ModelAndView showList() {
+        ModelAndView modelAndView = new ModelAndView("list");
+        List<Customer> customers = customerService.findAll();
+        modelAndView.addObject("customers", customers);
+        return modelAndView;
+    }
+
+    @GetMapping("customers/{id}")
+    public ModelAndView showInformation(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("info");
+        Customer customer = customerService.findById(id);
+        modelAndView.addObject("customer", customer);
+        return modelAndView;
+    }
+
+    @PostMapping("/customers")
+    public String updateCustomer(Customer customer) {
+        customerService.save(customer);
+        return "redirect:/customers";
+    }
+}
---
+++ customer/demo/src/main/java/com/example/demo/service/ICustomerService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    Customer findById(Long id);
+
+    void save(Customer customer);
+}
